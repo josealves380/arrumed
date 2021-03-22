3c875f747e2de4344d095a32e31417d54ca294a9
josealves380 <netopereira464@gmail.com>
2021-03-21 23:03:46 -0300

    Definindo componente Liberação

--- src/components/liberacao/Liberacao.js
+++ src/components/liberacao/Liberacao.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Menu from '../../widgest/Navbar/index'
 
 export default props =>( 
     <div>
+        <Menu />
         <h1>Liberação da Medicação</h1>
+        
+            <div>
+            <input type="text" placeholder="Informe o nome do Médico que liberou a medicação"></input>
+            </div>
     </div>
 )
